36f782aaa0228a5c89119c25549b2751afdb8b81
KERDANET Loic <loic943@gmail.com>
2021-11-12 07:53:01 +0100

    ajout object-manager.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -20,7 +20,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @ORM\Column(type="integer")
      * @var int|null
      */
-    private $id; /** @phpstan-ignore-line */
+    private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
